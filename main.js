b3636e90013576ca3dd86b5fc38c282e93830ad4
Brandon Zhang <75633537+iambran@users.noreply.github.com>
2022-03-02 21:03:10 +0800

    web components 2

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-class customerTestimonial extends HTMLElement {
+class CustomerTestimonial extends HTMLElement {
     constructor() {
         super();
 
+        // 附加一个shadow root
+        this.attachShadow({mode: 'open'});
         this.template = document.createElement('template');
         this.template.innerHTML = `
         <div class="testimonial">
             <div class="testimonial__content">
-            "“Brandon's CSS Grid tutorial is awesome. It covers so many details. Love that you use diagram from Figma to explain those details.”"
+            “Brandon's CSS Grid tutorial is awesome. It covers so many details. Love that you use diagram from Figma to explain those details.”
             </div>
             <div class="testimonial__footer">
-            <img alt="avatar" src="https://tse4-mm.cn.bing.net/th/id/OIP-C.sKod7sWUm96BbYoiIzsLDgD6D6?pid=ImgDet&rs=1" class="testimonial__avatar">
+            <img alt="avatar" src="./img/avatar.jpg" class="testimonial__avatar">
             <div>
                 <div class="testimonial__name">
-                Sienna Lee
+                    Sienna Lee
                 </div>
                 <div class="testimonial__role">
-                Web Administrator
+                    Web Administrator
                 </div>
             </div>
             </div>
@@ -32,7 +34,7 @@ class customerTestimonial extends HTMLElement {
             cursor: pointer;
             padding: 4vmin 8vmin;
             border-radius: 20px;
-            box-shadow: 0 2px 40px rgba(0 0 0 / 15%);
+            box-shadow: 0 2px 40px rgba(0 0 0 / 25%);
           }
           
         .testimonial__content {
@@ -62,15 +64,15 @@ class customerTestimonial extends HTMLElement {
             font-size: 14px;
         }
         `;
-
-        
     }
 
     // connectedCallback() 生命周期回调函数
     connectedCallback() {
-        this.appendChild(this.template.content);
-        this.appendChild(this.styles);
+        // this.appendChild(this.template.content);
+        // this.appendChild(this.styles);
+        this.shadowRoot.appendChild(this.template.content);
+        this.shadowRoot.appendChild(this.styles);
     }
 }
 
-customElements.define('customer-testimonial', customerTestimonial);
+customElements.define('customer-testimonial', CustomerTestimonial);
